9b39d9e2fe3969d6813e1c76fead7198c148ef62
Rita A <35456@ufp.edu.pt>
2023-03-13 11:57:32 +0000

    05_observer update

---
+++ src/edu/ufp/inf/sd/rmi/_05_observer/server/State.java
@@ -0,0 +1,31 @@
+package edu.ufp.inf.sd.rmi._05_observer.server;
+
+import java.io.Serializable;
+
+public class State  implements Serializable {
+
+    private String msg;
+
+    private String id;
+
+    public State(String msg, String id) {
+        this.msg = msg;
+        this.id = id;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
