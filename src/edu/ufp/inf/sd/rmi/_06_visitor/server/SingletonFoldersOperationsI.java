6d3cb6295b57e2c776b1cca95eb442863e987187
Rita A <35456@ufp.edu.pt>
2023-03-30 11:54:29 +0100

    06_visitor update

---
+++ src/edu/ufp/inf/sd/rmi/_06_visitor/server/SingletonFoldersOperationsI.java
@@ -0,0 +1,8 @@
+package edu.ufp.inf.sd.rmi._06_visitor.server;
+
+public interface SingletonFoldersOperationsI {
+
+    public Boolean createFile(String fname);
+
+    public Boolean deleteFile(String fname);
+}
